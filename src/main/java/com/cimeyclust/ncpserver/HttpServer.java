fc0ca247958bac153d8a5afa9f11e36fd2712a3c
Verox001 <altergeist987@gmail.com>
2023-05-06 21:37:30 +0200

    Added essentials

---
+++ src/main/java/com/cimeyclust/ncpserver/HttpServer.java
@@ -0,0 +1,29 @@
+package com.cimeyclust.ncpserver;
+
+import com.google.gson.Gson;
+import fi.iki.elonen.NanoHTTPD;
+
+public class HttpServer extends NanoHTTPD {
+    private Gson gson;
+
+    public HttpServer(String hostname, int port) {
+        super(hostname, port);
+        gson = new Gson();
+    }
+
+    @Override
+    public Response serve(IHTTPSession session) {
+        String responseBody = "";
+        int code = 200;
+        switch (session.getUri()) {
+            case "/":
+                responseBody = "{}";
+                break;
+            default:
+                code = 404;
+                responseBody = "{\"error\":\"404 Not Found\"}";
+        }
+
+        return newFixedLengthResponse(Response.Status.lookup(code), "application/json", responseBody);
+    }
+}
--- src/main/java/com/cimeyclust/ncpserver/Plugin.java
+++ src/main/java/com/cimeyclust/ncpserver/Plugin.java
@@ -1,18 +1,38 @@
 package com.cimeyclust.ncpserver;
 
 import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.utils.Config;
 import net.catrainbow.nocheatplus.NoCheatPlus;
-import net.catrainbow.nocheatplus.components.NoCheatPlusAPI;
+
+import java.io.IOException;
 
 public class Plugin extends PluginBase {
     private static Plugin instance;
-
-    public static NoCheatPlus provider;
+    private NoCheatPlus ncp;
+    private Config config;
+    private HttpServer server;
 
     @Override
     public void onLoad() {
         instance = this;
         getLogger().info("NCP Webserver is starting up...");
+
+        // Config
+        getLogger().info("Saving and reading config...");
+        saveResource("config.yml", false);
+        config = new Config(getDataFolder() + "/config.yml", Config.YAML);
+
+        // HTTP Server
+        getLogger().info("Starting up HTTP Server...");
+        server = new HttpServer(config.getString("hostname"), config.getInt("port"));
+        try {
+            server.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+            getLogger().error("§cError while starting HTTP Server! Please make sure that the port is not already in use.");
+            getLogger().info("§cDisabling NCP Webserver...");
+            this.getServer().getPluginManager().disablePlugin(this);
+        }
     }
 
     @Override
@@ -23,13 +43,34 @@ public class Plugin extends PluginBase {
             this.getServer().getPluginManager().disablePlugin(this);
         } else {
             getLogger().info("§aNCP Plugin found!");
-            provider = NoCheatPlus.instance;
+            ncp = NoCheatPlus.instance;
         }
-        getLogger().info("NCP Webserver is now accessible via !");
+        getLogger().info("NCP Webserver is now accessible via http://" + config.getString("hostname") + ":" + config.getInt("port"));
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("NCP Server is disabled!");
+        getLogger().info("Stopping HTTP Server...");
+        if (server != null) {
+            server.stop();
+            getLogger().info("§aStopped HTTP Server!");
+        }
+        getLogger().info("§cNCP Server has been disabled!");
+    }
+
+    public NoCheatPlus getNcp() {
+        return ncp;
+    }
+
+    public Config getConfig() {
+        return config;
+    }
+
+    public HttpServer getHttpServer() {
+        return server;
+    }
+
+    synchronized public static Plugin getInstance() {
+        return instance;
     }
 }
